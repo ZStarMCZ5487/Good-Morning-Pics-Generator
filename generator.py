04fd88009cfe37fa2cb0f751a69078e968be26a4
ZStarMCZ5487 <73019806+ZStarMCZ5487@users.noreply.github.com>
2025-04-01 15:17:20 +0800

    auto install requirement

--- generator.py
+++ generator.py
@@ -1,3 +1,5 @@
+import subprocess
+import sys
 import textwrap
 
 from PIL import Image, ImageDraw, ImageFont
@@ -11,6 +13,16 @@ import random
 #    image_url = "https://api.waifu.pics/sfw/waifu"
 #else:
 #    image_url = give me husbando version... pls 👉👈🥺
+
+def install_requirements():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+        print("All required packages installed successfully!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+install_requirements()
+
 
 root = tk.Tk()
 root.title("Good Morning Pics Generator")
